9f3e26092eefc230f154a8f573450b7fb8055577
Jeremy Black <jb@yodacom.com>
2017-04-13 08:12:02 -0600

    split names for jedi

--- hello_server.js
+++ hello_server.js
@@ -9,12 +9,14 @@ app.get('/:firstname/:lastname', function(request, response){
 	var first = request.params.firstname;
 	var last = request.params.lastname;
 	response.send(["Hello", first, last].join(" "));
-
-app.get('/:firstname/:lastname', function(request, response){
-	var first = request.params.firstname;
-	var last = request.params.lastname;
-	response.send(["Hello", first, last].join(" "));
-
 });
 
+app.get('/jedi/:firstname/:lastname', function(request, response){
+	var first = request.params.firstname.slice(0,2);
+	var last = request.params.lastname.slice(0,3);
+	response.send(["Hello", last, first].join(" "));
+});
+
+
 app.listen(process.env.PORT || 8080, process.env.IP);
+
